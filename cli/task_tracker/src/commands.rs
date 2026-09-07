0df9b4c472ea90478ec404da690c59f6e56fc906
Marciano Mavungo <marcmav@proton.me>
2026-09-07 23:01:54 +0100

    add file to save tasks

--- cli/task_tracker/src/commands.rs
+++ cli/task_tracker/src/commands.rs
@@ -1,5 +1,7 @@
 pub mod manage_task {
+    /*
     pub fn add(task: &str) -> bool {
 
     }
+    */
 }
--- cli/task_tracker/src/main.rs
+++ cli/task_tracker/src/main.rs
@@ -1,5 +1,15 @@
 mod commands;
 
-fn main() {
+use std::fs::File;
+use std::io::ErrorKind;
 
+#[allow(unused_variables)] // cant go in production;
+fn main() {
+    let file = match File::open("TODO") {
+        Ok(fc) => fc,
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => File::open("TODO").expect("File doesnt exist and cant create."),
+            _ => panic!("An error occured.")
+        }
+    };
 }
