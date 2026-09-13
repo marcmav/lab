e0de6f0857c2078ccdda38bfc0f70b4698f3a19d
Marciano Mavungo <marcmav@proton.me>
2026-09-13 17:28:38 +0100

    add init_task_tracker to prompt the initial content

---
+++ cli/task_tracker/src/lib.rs
@@ -0,0 +1,17 @@
+mod commands;
+
+pub fn init_task_tracker() {
+    println!("==================================================");
+    println!("                 Task Tracker");
+    println!("==================================================");
+    println!();
+    println!("--------------------------------------------------");
+    println!("                   Commands:");
+    println!("(type the command's name or the number it was assign)");
+    println!("--------------------------------------------------");
+    println!("1 - Add -- (add a new task)");
+    println!("2 - Update -- (update a task)");
+    println!("3 - Delete -- (delete a task)");
+    println!("--------------------------------------------------");
+}
+
--- cli/task_tracker/src/main.rs
+++ cli/task_tracker/src/main.rs
@@ -1,15 +1,7 @@
-mod commands;
+#[warn(dead_code)] //cant go in production;
 
-use std::fs::File;
-use std::io::ErrorKind;
+use task_tracker::*;
 
-#[allow(unused_variables)] // cant go in production;
 fn main() {
-    let file = match File::open("TODO") {
-        Ok(fc) => fc,
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => File::open("TODO").expect("File doesnt exist and cant create."),
-            _ => panic!("An error occured.")
-        }
-    };
+    init_task_tracker();
 }
