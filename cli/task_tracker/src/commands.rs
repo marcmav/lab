da54fbf9ad642662a053d0fafa9f0f77cfea8bf8
Marciano Mavungo <marcmav@proton.me>
2026-09-13 17:34:56 +0100

    begin ADD command

--- cli/task_tracker/src/commands.rs
+++ cli/task_tracker/src/commands.rs
@@ -1,7 +1,7 @@
 pub mod manage_task {
-    /*
-    pub fn add(task: &str) -> bool {
-
+    pub fn add(task: String) {
+        println!("Type the task you want to add.");
+        //the logic goes here;
+        println!("Success: Created a new task.");
     }
-    */
 }
