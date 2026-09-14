88ebf6a4652dd0b40255de1bba004c88b5a7e08e
Marciano Mavungo <marcmav@proton.me>
2026-09-14 22:38:32 +0100

    add feature - accept user input

--- cli/task_tracker/src/main.rs
+++ cli/task_tracker/src/main.rs
@@ -1,7 +1,11 @@
 #[warn(dead_code)] //cant go in production;
 
 use task_tracker::*;
+use std::env;
 
 fn main() {
-    init_task_tracker();
+    let args: Vec<String> =env::args().skip(1).collect();
+    if args.len() == 0 {
+        init_task_tracker();
+    }
 }
