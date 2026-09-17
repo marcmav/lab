573d3d24c8517a66793675c62398dbbcb621f80c
Marciano Mavungo <marcmav@proton.me>
2026-09-17 16:55:31 +0100

    finish game

--- cli/guessing_game/src/main.rs
+++ cli/guessing_game/src/main.rs
@@ -1,44 +1,59 @@
-mod utils;
-
-use utils::*;
-use std::io;
+use std::io::{self, Write};
 use rand::Rng;
 
 fn main() {
-    greet_user();
-    let attempt = match select_difficulty().trim() {
-        "1" | "easy" => {
-            println!("You chose easy difficulty");
-            20 },
-        "2" | "medium" => {
-            println!("You chose medium difficulty");
+    println!("\nWelcome to the Number Guessing Game!");
+    println!("I'm thinking of a number between 1 and 100.");
+    println!("You have a number of chances to guess the correct number.\n");
+    println!("Please select the difficulty level:");
+    println!("1. Easy (10 chances)");
+    println!("2. Medium (5 chances)");
+    println!("3. Hard (3 chances)");
+    println!("4. Impossible (1 chance)");
+    println!("5. Quit\n");
+    print!("Enter you choice: ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("An error occured while trying to define the difficulty level");
+    let chances = match input.trim() {
+        "1" | "Easy" => {
+            println!("\nGreat! You have selected the Easy difficulty level.");
             10 },
-        "3" | "hard" => {
-            println!("You chose hard difficulty");
+        "2" | "Medium" => {
+            println!("\nGreat! You have selected the Medium difficulty level.");
             5 },
-        "4" | "impossible" => {
-            println!("You chose impossible difficulty");
+        "3" | "Hard" => {
+            println!("\nGreat! You have selected the Hard difficulty level.");
+            3 },
+        "4" | "Impossible" => {
+            println!("\nGreat! You have selected the Impossible difficulty level.");
             1 },
-        "5" | "quit" => {
-            println!("Bye bye");
+        "5" | "Quit" => {
+            println!("\nBye bye");
             return; },
-        _ => 0,
+        _ => { panic!("Wrong input"); },
     };
-    println!("You have {} attempt/s to get it right", attempt);
-    println!("Choose a number (between 1 and 100): ");
+    println!("Let's start the game!\n");
     let random_number: u32 = rand::thread_rng().gen_range(1..=100);
-    for i in 1..=attempt {
+    for i in 1..=chances {
+        print!("Enter your guess: ");
+        io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("An error occured while getting the guess");
+        if let "Quit" | "Q" | "q" = input.trim() {
+            println!("Bye bye");
+            return ;
+        }
         let guess: u32 = input.trim().parse().expect("Not a valid number");
         if guess == random_number {
-            println!("You guessed it right, congratulations!");
-            println!("You used {i} attempts, the were {} left", attempt - i);
+            println!("Congratulations! You guessed the correct number in {} attempts", i);
             return;
         } else if guess > random_number {
-            println!("You guessed to high");
+            println!("Incorrect! The number is less than {}\n", guess);
         } else if guess < random_number {
-            println!("You guessed to low");
+            println!("Incorrect! The number is greater than {}\n", guess);
         }
     }
     println!("You ran out of attempts, you lost!");
