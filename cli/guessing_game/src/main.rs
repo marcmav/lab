565d020d38b787f643b807f347d682f005a2a155
Marciano Mavungo <marcmav@proton.me>
2026-09-17 15:46:42 +0100

    update - feedback after winning or losing

--- cli/guessing_game/src/main.rs
+++ cli/guessing_game/src/main.rs
@@ -6,7 +6,7 @@ use rand::Rng;
 
 fn main() {
     greet_user();
-    let mut attempt = match select_difficulty().trim() {
+    let attempt = match select_difficulty().trim() {
         "1" | "easy" => {
             println!("You chose easy difficulty");
             20 },
@@ -27,19 +27,20 @@ fn main() {
     println!("You have {} attempt/s to get it right", attempt);
     println!("Choose a number (between 1 and 100): ");
     let random_number: u32 = rand::thread_rng().gen_range(1..=100);
-    while attempt > 0 {
+    for i in 1..=attempt {
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("An error occured while getting the guess");
         let guess: u32 = input.trim().parse().expect("Not a valid number");
         if guess == random_number {
             println!("You guessed it right, congratulations!");
+            println!("You used {i} attempts, the were {} left", attempt - i);
             return;
         } else if guess > random_number {
             println!("You guessed to high");
         } else if guess < random_number {
             println!("You guessed to low");
         }
-        attempt -= 1;
     }
     println!("You ran out of attempts, you lost!");
+    println!("The random number was {random_number}");
 }
