cb137cb8c2b881265d297457bba2365c0b95e311
Marciano Mavungo <marcmav@proton.me>
2026-09-17 15:07:39 +0100

    update

--- cli/task_tracker/src/main.rs
+++ cli/task_tracker/src/main.rs
@@ -2,8 +2,11 @@
 
 use task_tracker::*;
 use std::env;
+use std::fs::File();
+use std::io::{self, Read, Write, BufReader, BufWriter};
 
-fn main() {
+fn main() -> Result<String, io::Error> {
+    let f = File::open("TODO")?;
     let args: Vec<String> = env::args().skip(1).collect();
     //TODO accept input as the numbers assigned
     //TODO accept multiple parameters later
