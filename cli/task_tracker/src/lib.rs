db5bb8dce37030918031c0107edfa809b1dbc7cb
Marciano Mavungo <marcmav@proton.me>
2026-09-20 17:20:55 +0100

    add task structure and status enum

--- cli/task_tracker/src/lib.rs
+++ cli/task_tracker/src/lib.rs
@@ -1,1 +1,15 @@
 pub mod utils;
+
+enum Status {
+    todo,
+    in_progress,
+    done,
+}
+
+struct Task {
+    id: u32,
+    description: String,
+    status: Status,
+    created_at: (u8; 3),
+    updated_at: (u8, 3),
+}
